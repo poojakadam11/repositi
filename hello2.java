64195534f3ebd5a8bb8008a6718d6d203a170850
poojakadam11 <90841384+poojakadam11@users.noreply.github.com>
2021-09-22 14:27:47 +0530

    new program

--- hello2.java
+++ hello2.java
@@ -1,1 +1,14 @@
-welcome
+package basic;
+
+public class Split {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s1="Java strings methods";
+String [] w=s1.split("\\s");
+for(String words:w) {
+	System.out.println(words);
+}
+	}
+
+}
